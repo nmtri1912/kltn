12b499f7f1794a45b394804f70cdea7635838065
Minh Trí <nguyenminhtricpr@gmail.com>
2020-03-21 20:28:21 +0700

    Update app.py

--- PoC/app.py
+++ PoC/app.py
@@ -4,11 +4,7 @@ from tensorflow import keras
 
 app = Flask('stock_pricer')
 
-
 #-----
-
-import tensorflow as tf
-from tensorflow import keras
 from keras.models import load_model
 import numpy as np
 from pickle import load
@@ -16,8 +12,13 @@ from numpy import argmax
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+import keras.backend.tensorflow_backend as tb
+tb._SYMBOLIC_SCOPE.value = True
 
-model = load_model('model.h5')
+
+sess = tf.compat.v1.Session()
+graph = tf.compat.v1.get_default_graph()
+
 #model.summary()
 
 #clean data
@@ -71,41 +72,45 @@ def word_for_id(integer, tokenizer):
 
 # generate target given source sequence
 def predict_sequence(model, tokenizer, source):
-	prediction = model.predict(source, verbose=0)[0]
-	integers = [argmax(vector) for vector in prediction]
-	target = list()
-	for i in integers:
-		word = word_for_id(i, tokenizer)
-		if word is None:
-			break
-		target.append(word)
-	return ' '.join(target)
+	with sess.as_default():
+		with graph.as_default():
+			prediction = model.predict(source, verbose=0)[0]
+			integers = [argmax(vector) for vector in prediction]
+			target = list()
+			for i in integers:
+				word = word_for_id(i, tokenizer)
+				if word is None:
+					break
+				target.append(word)
+			return ' '.join(target)
 #----
 
 @app.route('/')
 def show_predict_stock_form():
-    return render_template('predictorform.html')
+	return render_template('predictorform.html')
+
+
 @app.route('/result', methods=['POST'])
 def results():
-    form = request.form
-    if request.method == 'POST':
-      #write your function that loads the model
-       #model = get_model() #you can use pickle to load the trained model
-        year = request.form['year']
-        #predicted
-        tk2 = create_tokenizer(year)
-        year = year.split(" ")
-        predicted = list()
-        for text in year:
-            tk = create_tokenizer(text)
-            source = encode_sequences(tk,ger_length,text)
-            temp = predict_sequence(model,eng_tokenizer,source) + ' '
-            predicted.append(temp)
+	form = request.form
+	if request.method == 'POST':
+		with sess.as_default():
+			with graph.as_default():
+				model = tf.keras.models.load_model("model.h5")
+				year = request.form['year']
+				tk2 = create_tokenizer(year)
+				year = year.split(" ")
+				predicted = list()
+				for text in year:
+					tk = create_tokenizer(text)
+					source = encode_sequences(tk,ger_length,text)
+					temp = predict_sequence(model,eng_tokenizer,source) + ' '
+					predicted.append(temp)
 
-        predicted_stock_price = predicted
+				predicted_stock_price = predicted
 
-        #
-        return render_template('resultsform.html', year=year, predicted_price=predicted_stock_price)
+		#
+		return render_template('resultsform.html', year=year, predicted_price=predicted_stock_price)
 
 
 app.run("localhost", "9999", debug=True)
