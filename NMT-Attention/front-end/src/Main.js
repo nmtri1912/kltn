70cc630f70b418eba7b6f0e086ef92c2b9ca7966
tientruongphamnhat <49446415+tientruongphamnhat@users.noreply.github.com>
2020-06-18 09:13:28 +0700

    tai lieu trien khai, huong dan cai dat release, ban demo

--- NMT-Attention/front-end/src/Main.js
+++ NMT-Attention/front-end/src/Main.js
@@ -31,13 +31,14 @@ class Main extends React.Component {
     this.setState({
       isLoading: true,
     });
-
-    fetch('http://localhost:9999/translate', {
-      //fetch('http://3.21.198.225/translate', {
+    fetch(' http://35.240.242.108/translate', {
+      //fetch('http://localhost:9999/translate', {
+      //fetch('http://3.21.179.138/translate', {
       method: 'post',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
       },
 
       body: JSON.stringify({
@@ -67,65 +68,79 @@ class Main extends React.Component {
   }
 
   render() {
+    const { input, output, isLoading } = this.state;
     return (
       <>
-        <div className="col-xl-10 container" style={{marginTop:'20px'}}>
-          <div>
-            <div className="text-center">
-              <h1>Demo Mô Hình Dịch Máy Từ Tiếng Anh Sang Tiếng Việt</h1>
-            </div>
-          </div>
-          <div className="row mt-5 text-center">
-            <div className="col" style={{ fontSize: '28px' }}>
-              Tiếng Anh
-            </div>
-            <div className="col" style={{ fontSize: '28px' }}>
-              Tiếng Việt
-            </div>
-          </div>
-          <div className="row mt-2 text-center">
-            <div className="col" style={{ minHeight: '200px', height: 'auto' }}>
-              <TextareaAutosize
-                style={{
-                  minHeight: '100%',
-                  minWidth: '100%',
-                  height: 'auto',
-                  resize: 'vertical',
-                }}
-                name="input"
-                placeholder="Enter text"
-                autoCapitalize="off"
-                autoComplete="off"
-                autoCorrect="off"
-                spellCheck="false"
-                value={this.state.input}
-                onChange={this.handlechange}
-              ></TextareaAutosize>
-            </div>
+        <div className="container text-center">
+          <p
+            style={{
+              fontSize: '30px',
+              marginTop: '15px',
+              color: '#006EFF',
+            }}
+          >
+            Demo Mô Hình Dịch Máy Từ Tiếng Anh Sang Tiếng Việt
+          </p>
 
-            <div className="col" style={{ minHeight: '200px' }}>
-              <TextareaAutosize
-                value={this.state.output}
-                onChange={this.handlechange}
-                name="output"
-                readOnly="true"
-                style={{ minHeight: '100%', minWidth: '100%' }}
-              ></TextareaAutosize>
+          <div className="row mt-4">
+            <div className="col-sm-6 text-center">
+              <p style={{ fontSize: '28px' }}>Tiếng Anh</p>
+              <div style={{ minHeight: '200px', height: 'auto' }}>
+                <TextareaAutosize
+                  style={{
+                    minHeight: '200px',
+                    minWidth: '100%',
+                    height: 'auto',
+                    resize: 'vertical',
+                    fontSize: '20px',
+                    borderColor: '#D3D3D3',
+                    padding: '10px',
+                  }}
+                  name="input"
+                  placeholder="Nhập nội dung"
+                  autoCapitalize="off"
+                  autoComplete="off"
+                  autoCorrect="off"
+                  spellCheck="false"
+                  value={input}
+                  onChange={this.handlechange}
+                ></TextareaAutosize>
+              </div>
+            </div>
+            <div className="col-sm-6 text-center">
+              <p style={{ fontSize: '28px' }}>Tiếng Việt</p>
+              <div style={{ minHeight: '200px' }}>
+                <TextareaAutosize
+                  value={output}
+                  onChange={this.handlechange}
+                  name="output"
+                  readOnly={true}
+                  style={{
+                    minHeight: '200px',
+                    minWidth: '100%',
+                    borderColor: '#D3D3D3',
+                    padding: '10px',
+                  }}
+                ></TextareaAutosize>
+              </div>
             </div>
           </div>
-          <div className="text-center mt-3">
-            {this.state.isLoading ? (
-              <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-              </Spinner>
-            ) : (
-              <button
-                onClick={this.handleTranslate}
-                className="btn btn-secondary"
-              >
-                Dịch
-              </button>
-            )}
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="center mt-3 col-sm-6">
+              {isLoading ? (
+                <Spinner animation="border" role="status">
+                  <span className="sr-only">Loading...</span>
+                </Spinner>
+              ) : (
+                <button
+                  onClick={this.handleTranslate}
+                  className="btn btn-secondary"
+                  style={{ width: '100%', background: '#006EFF' }}
+                >
+                  Dịch
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </>
